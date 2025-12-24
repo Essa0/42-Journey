d7a17ab8db4c8ff7ca1725b13d3b3b1cc715a640
Essa0 <ealobisat@gmail.com>
2025-12-24 14:57:28 +0300

    update: main file

--- pushswap/main.c
+++ pushswap/main.c
@@ -6,7 +6,7 @@
 /*   By:  iabiesat < iabiesat@student.42amman.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 17:48:48 by  iabiesat         #+#    #+#             */
-/*   Updated: 2025/12/22 02:04:36 by  iabiesat        ###   ########.fr       */
+/*   Updated: 2025/12/24 03:06:12 by  iabiesat        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,74 +22,97 @@
 // 	printf("\n");
 // }
 
-int	main(int argc, char **argv)
+static void	clean_fun(char **part, int *numbers, t_node *stack_a, t_node *stack_b)
 {
-	int count = 0;
-	int *numbers;
-	char **part;
+	if (part)
+		free_full_arr(part);
+	if (numbers)
+		free(numbers);
+	if (stack_a)
+		free_stack(stack_a);
+	if (stack_b)
+		free_stack(stack_b);
+}
+static void sort_stack(t_node **stack_a, t_node **stack_b, int count)
+{
+	idx_stack(stack_a);
+	if (count == 2)
+		s_operation("sa", stack_a, stack_b);
+	else if (count > 2 && count <= 5)
+		simple_sort(stack_a, stack_b);
+	else if (count > 5)
+		sort_algo(stack_a, stack_b);
+}
+static void init_and_sort(char **part, int *number, int count)
+{
 	t_node *stack_a;
 	t_node *stack_b;
+	
+	stack_a = init_stack(number, count);
+	if (!stack_a)
+	{
+		clean_fun(part, number, NULL, NULL);
+		write(2, "Error\n", 6);
+		return ;
+	}
+	stack_b = NULL;
+	if (is_sorted(stack_a))
+	{
+		clean_fun(part, number, stack_a, stack_b);
+		return ;
+	}
+	sort_stack(&stack_a, &stack_b, count);
+	clean_fun(part, number, stack_a, stack_b);
+}
 
-	if (argc > 1)
+static int valid_fun(char **part, int **number, int *count)
+{
+	if (!validateInput(part))
+	{
+		free_full_arr(part);
+		write(2, "Error\n", 6);
+		return (0);
+	}
+	*number = convert_to_integer(part, count);
+	if (!*number)
+	{
+		free_full_arr(part);
+		write(2, "Error\n", 6);
+		return (0);
+	}
+	if (!check_for_duplicate(*number, *count))
+	{
+		free(*number);
+		free_full_arr(part);
+		write(2, "Error\n", 6);
+		return (0);
+	}
+	
+	return (1);	
+}
+
+
+int	main(int argc, char **argv)
+{
+	int count;
+	int *numbers;
+	char **part;
+	
+	if(argc > 1)
 	{
 		part = split_and_store(argc, argv);
 		if (!part || !part[0])
 		{
-			write(1, "Error in split\n", 6);
+			if (part)
+				free_full_arr(part);
+			write(2, "Error\n", 6);
+			return(1);
+		}
+		count = 0;
+		numbers = NULL;
+		if (!valid_fun(part, &numbers, &count))
 			return (1);
-		}
-		if (!validateInput(part))
-		{
-			free_full_arr(part);
-			write(1, "Error in validation\n", 6);
-			return (1);
-		}
-
-		numbers = convert_to_integer(part, &count);
-
-		if (!numbers)
-		{
-			printf("Error Numbers\n");
-			free_full_arr(part);
-			return (1);
-		}
-		if (!check_for_duplicate(numbers, count))
-		{
-			printf("there are duplicate");
-			free(numbers);
-			free_full_arr(part);
-			return (1);
-		}
-
-		stack_a = init_stack(numbers, count);
-		if (!stack_a)
-		{
-			free(numbers);
-			free_full_arr(part);
-		}
-		stack_b = NULL;
-
-		if (is_sorted(stack_a))
-		{
-			free_stack(stack_a);
-			free(numbers);
-			free_full_arr(part);
-			return (0);
-		}
-
-		idx_stack(&stack_a);
-		if (count == 2)
-			s_operation("sa", &stack_a, &stack_b);
-		else if (count > 2 && count <= 5)
-			simple_sort(&stack_a, &stack_b);
-		else if(count > 5)
-			sort_algo(&stack_a, &stack_b);
-
-		free_stack(stack_a);
-		if (stack_b)
-			free_stack(stack_b);
-		free(numbers);
-		free_full_arr(part);
+		init_and_sort(part, numbers, count);
 	}
 	return (0);
 }
