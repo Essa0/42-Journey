a5130f14cbbbeccb4d66fe187f4a292f6b0d86ef
Essa0 <ealobisat@gmail.com>
2026-01-18 17:16:19 +0300

    update main: validate the input

--- fract-ol/main.c
+++ fract-ol/main.c
@@ -6,16 +6,11 @@
 /*   By: iabiesat <iabiesat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 04:04:37 by  iabiesat         #+#    #+#             */
-/*   Updated: 2026/01/18 13:45:36 by iabiesat         ###   ########.fr       */
+/*   Updated: 2026/01/18 17:15:16 by iabiesat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-
-static void	error_msg(void)
-{
-	write(1, "Error", 5);
-}
 
 static void	init_img(t_img *img)
 {
@@ -35,29 +30,69 @@ static void	init_hooks(t_img *img)
 	mlx_hook(img->win_ptr, 17, 0, close_window, img);
 }
 
+static int	is_valid_double(char *str)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == '.')
+		{
+			count++;
+			if (count > 1)
+				return (0);
+		}
+		else if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	parse_args(int ac, char **av, t_img *img)
+{
+	if (ac == 2 && !check_input(av[1], "mandelbrot"))
+	{
+		img->fractal_type = 0;
+		return (1);
+	}
+	if (ac == 4 && !check_input(av[1], "julia"))
+	{
+		if (is_valid_double(av[2]) && is_valid_double(av[3]))
+		{
+			img->fractal_type = 1;
+			img->j_re = ft_atof(av[2]);
+			img->j_im = ft_atof(av[3]);
+			return (1);
+		}
+	}
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	t_img	img;
 
-	if ((ac == 2 && check_input(av[1], "mandelbrot") == 0)
-		|| (ac == 4 && check_input(av[1], "julia") == 0))
+	if (parse_args(ac, av, &img))
 	{
 		img.mlx_ptr = mlx_init();
 		img.win_ptr = mlx_new_window(img.mlx_ptr, WIDTH, HEIGHT, "fract-ol");
-		if (ac == 2)
-			img.fractal_type = 0;
-		else
-		{
-			img.fractal_type = 1;
-			img.j_re = ft_atof(av[2]);
-			img.j_im = ft_atof(av[3]);
-		}
 		init_img(&img);
 		render_draw(&img);
 		init_hooks(&img);
 		mlx_loop(img.mlx_ptr);
 	}
 	else
-		error_msg();
+	{
+		write(1, "Error", 5);
+		return (1);
+	}
 	return (0);
 }
